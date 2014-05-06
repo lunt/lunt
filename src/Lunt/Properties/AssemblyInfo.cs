ae96373671586d1049884e876bd5f02be4cb2d21
Patrik Svensson <patrik@patriksvensson.se>
2014-05-06 16:04:21 +0200

    Updated assembly information.

--- src/Lunt/Properties/AssemblyInfo.cs
+++ src/Lunt/Properties/AssemblyInfo.cs
@@ -1,16 +1,12 @@
 ﻿using System;
 using System.Reflection;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 using System.Runtime.CompilerServices;
 
-[assembly: AssemblyTitle("Lunt.Core")]
+[assembly: AssemblyTitle("Lunt")]
 [assembly: AssemblyDescription("")]
-
-// We're CLS compliant.
-[assembly: CLSCompliant(true)]
 
 // Expose internals to the test projects.
 [assembly: InternalsVisibleTo("Lunt.Tests")]
+
+// We're CLS compliant.
+[assembly: CLSCompliant(true)]
