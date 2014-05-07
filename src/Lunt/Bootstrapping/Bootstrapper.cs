3bd0ae206b84f66a8f33cdae7f27aa0fca684978
Patrik Svensson <patrik@patriksvensson.se>
2014-05-07 20:42:25 +0200

    Added missing XML comment.

--- src/Lunt/Bootstrapping/Bootstrapper.cs
+++ src/Lunt/Bootstrapping/Bootstrapper.cs
@@ -15,6 +15,9 @@ namespace Lunt.Bootstrapping
         /// <value>The application container.</value>
         public TContainer ApplicationContainer { get; protected set; }
 
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+        /// </summary>
         public void Dispose()
         {            
             Dispose(true);
